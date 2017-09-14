38d105ed57d61df7649ad66570859066f0ede23c
Helfer Thomas <thomas.helfer@cea.fr>
2017-09-14 22:41:58 +0200

    Change parameters to material properties

--- cmake/modules/cyrano.cmake
+++ cmake/modules/cyrano.cmake
@@ -3,3 +3,11 @@ if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
 else( CMAKE_SIZEOF_VOID_P EQUAL 8 )
   set(CYRANO_CPPFLAGS "-DCYRANO_ARCH=32")
 endif( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+
+function(check_cyrano_compatibility mat source)
+  behaviour_query(behaviour_type
+    ${mat} ${source} "--type")
+  if(NOT (behaviour_type EQUAL 1))
+    set(file_OK OFF PARENT_SCOPE)
+  endif(NOT (behaviour_type EQUAL 1))
+endfunction(check_cyrano_compatibility)
