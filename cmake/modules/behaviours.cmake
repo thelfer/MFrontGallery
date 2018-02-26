fa73dd30ceb3b2b161572fae775da9faa7830f58
Thomas Helfer <tfel-contact@cea.fr>
2018-02-26 10:38:49 +0100

    Port to Visual Studio

--- cmake/modules/behaviours.cmake
+++ cmake/modules/behaviours.cmake
@@ -61,7 +61,7 @@ function(mfront_behaviours_library mat)
       "${TFELException};${TFELMath};${TFELMaterial}")
   else((TFEL_CXX_STANDARD GREATER 17) OR (TFEL_CXX_STANDARD EQUAL 17))
     set(TFEL_MFRONT_LIBRARIES
-      "${TFELException};${TFELMath};${TFELMaterial};${TFELPhysicalConstants}")
+      "${TFELException};${TFELMath};${TFELMaterial};${TFELUtilities};${TFELPhysicalConstants}")
   endif((TFEL_CXX_STANDARD GREATER 17) OR (TFEL_CXX_STANDARD EQUAL 17))
   foreach ( _ARG ${ARGN})
     if ( ${_ARG} MATCHES SOURCES )
